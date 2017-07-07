e0d7366039c77916454fab2d99e9c1c5459f0cb0
root <root@61949b3d2f95>
2017-07-07 00:35:14 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_gepBZqOWny",
+  "homePage" : "Main",
+  "name" : "Auto_gepBZqOWny",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
